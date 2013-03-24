a2040ae1bde5e36b248f98438ca0cafe5243ae6c
Hunter Owens <owens.hunter@gmail.com>
2013-03-24 13:23:01 -0700

    doing some things

---
+++ routesToLocation.js
@@ -0,0 +1,10 @@
+print("test");
+
+var routesLength = 128;
+var apiKey = '9wXebQdvaJzpxdsr43DDjt3Pg';
+
+for (var i = routesLength - 1; i > 0; i--) {
+	var route = readline();
+	print(route);
+}
+
--- scraper.py
+++
@@ -1,7 +0,0 @@
-import urllib2
-
-f = open('routes.txt')
-lines = [line.strip() for line in open('routes.txt')]
-print lines
-
-print "imported"
